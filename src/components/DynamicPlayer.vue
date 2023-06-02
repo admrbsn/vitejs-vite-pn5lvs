b790a71a4fd4d6601615fa95fe83e8088946b737
admrbsn <admrbsn@gmail.com>
2023-06-02 08:37:17 -0600

    1 file modified, 4 added, 7 deleted

--- src/components/DynamicPlayer.vue
+++ src/components/DynamicPlayer.vue
@@ -1,11 +1,5 @@
 <template>
   <div class="dynamic-player">
-    <audio ref="audioRef" id="audio" playsinline>
-      <source
-        src="https://storage.googleapis.com/tribute-music-prod/205_full_clap-tap_0102.mp3"
-        type="audio/mpeg"
-      />
-    </audio>
     <swiper-container
       ref="swiperEl"
       noSwiping
@@ -15,9 +9,42 @@
       @init="onInit"
       @slidechange="onSlideChange"
     >
-      <div slot="container-start" class="p-5 text-white text-sm">
-        To: Recipienttt
+      <!-- Video header with Tribute info and mute button -->
+      <div
+        slot="container-start"
+        class="
+          relative
+          z-10
+          flex
+          items-center
+          justify-between
+          p-5
+          text-white text-sm
+          font-semibold
+        "
+      >
+        <!-- Tribute info -->
+        <div>To: Recipient</div>
+
+        <!-- Overaly for mobile interaction -->
+        <div v-show="showOverlay" class="overlay">
+          <div class="tooltip">Please unmute before playing.</div>
+        </div>
+
+        <!-- Mute/Unmute button -->
+        <button
+          @click="toggleMute"
+          class="flex items-center justify-center z-10"
+        >
+          <img
+            :src="isMuted ? volumeOffImage : volumeOnImage"
+            class="w-6 h-6"
+            alt="Mute/Unmute Button"
+          />
+        </button>
       </div>
+
+      <!-- Swiper slides -->
       <swiper-slide v-for="(slide, index) in slides" :key="index">
         <video
           ref="videoRefs"
@@ -25,48 +52,92 @@
           height="400"
           playsinline
           autobuffer
-          mute
+          :muted="isMuted"
           :id="'video-' + index"
         >
-          <source :src="slide.src" type="video/mp4" />
+          <source :src="slide.src" type="application/x-mpegURL" />
         </video>
+        <button
+          @click="togglePlay"
+          :class="{ 'hide-unless-hovered': hasStartedPlaying }"
+          class="
+            absolute
+            top-1/2
+            right-0
+            left-0
+            flex
+            w-16
+            h-16
+            items-center
+            justify-center
+            m-auto
+            bg-white
+            rounded-full
+            -translate-y-1/2
+          "
+        >
+          <img
+            :src="isPlaying ? pauseImage : playImage"
+            class="w-8 h-8"
+            :class="{ 'ml-1.5': !isPlaying }"
+            alt="Play/Pause Button"
+          />
+        </button>
       </swiper-slide>
     </swiper-container>
-    <button @click="playVideo" class="btn btn-primary btn-lg">Play</button>
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted, onBeforeUpdate } from 'vue';
+import { ref, onMounted, onUnmounted, nextTick } from 'vue';
 import { register } from 'swiper/element/bundle';
+import Hls from 'hls.js';
 register();
+
+// Mobile unmute helper
+const showOverlay = ref(false);
+
+// Ref for images
+const playImage = ref('play.svg');
+const pauseImage = ref('pause.svg');
+const volumeOnImage = ref('volume-on.svg');
+const volumeOffImage = ref('volume-off.svg');
 
 // Ref for swiper element
 const swiperEl = ref(null);
 const videoRefs = ref([]);
-const audioRef = ref(null);
+const currentPlayingIndex = ref(null);
+const isPlaying = ref(false);
+const isMuted = ref(true);
+const hasStartedPlaying = ref(false);
 const slides = [
   {
-    src: 'intro.mp4',
+    src: 'https://player.vimeo.com/external/823050002.m3u8?s=3f3e42fa89c4193ccc3e30707faf593eccbb9696',
   },
   {
-    src: 'jimmy.mp4',
+    src: 'https://player.vimeo.com/external/832639412.m3u8?s=af15996d49ebcb49d9444c3547919ad70256da15',
   },
   {
-    src: 'luke.mp4',
+    src: 'https://player.vimeo.com/external/832639348.m3u8?s=3f11e4fe435908857eb79bc44a44ddcd16a5733e',
   },
 ];
 
 onMounted(() => {
+  // Show tooltip helper to unmute on mobile
+  showOverlay.value = window.innerWidth < 768 && isMuted.value;
+  window.addEventListener('resize', () => {
+    showOverlay.value = window.innerWidth < 768 && isMuted.value;
+  });
+
   // Swiper parameters
   const swiperParams = {
     autoplay: false,
-    loop: true,
     injectStyles: [
       `
         :root {--swiper-theme-color: #fff;}
         swiper-container {height:100%;background-color:#202124;}
         swiper-slide {display:flex;align-items:center;justify-content:center;color:#fff;}
+        .swiper-button-next,.swiper-button-prev {z-index: 9;}
       `,
     ],
   };
@@ -74,6 +145,28 @@ onMounted(() => {
   // Assign all parameters to Swiper element and initialize it
   Object.assign(swiperEl.value, swiperParams);
   swiperEl.value.initialize();
+
+  // Iterate over slides
+  slides.forEach((slide, index) => {
+    // HLS
+    if (Hls.isSupported()) {
+      var hls = new Hls();
+      hls.loadSource(slide.src);
+      hls.attachMedia(videoRefs.value[index]);
+      videoRefs.value[index].onended = () => {
+        if (swiperEl.value && swiperEl.value.swiper) {
+          swiperEl.value.swiper.slideNext(); // slide to the next video
+        }
+      };
+    }
+  });
+});
+
+onUnmounted(() => {
+  // Remove event listener when component is unmounted to avoid memory leaks
+  window.removeEventListener('resize', () => {
+    showOverlay.value = window.innerWidth < 768 && isMuted.value;
+  });
 });
 
 // Methods
@@ -81,71 +174,86 @@ const onInit = (e) => {
   console.log('swiper initialized');
 };
 
-const onSlideChange = (e) => {
-  console.log('slide changed');
-  const swiper = swiperEl.value.swiper;
-  const currentIndex = e.detail[0].realIndex;
-  const previousIndex = swiper.previousIndex;
-
-  const previousVideo = videoRefs.value[previousIndex];
-  if (previousVideo) {
-    previousVideo.pause();
-    previousVideo.currentTime = 0;
-  }
-
-  const currentVideo = videoRefs.value[currentIndex];
-  currentVideo.muted = false;
-  currentVideo.play();
-
-  currentVideo.addEventListener('ended', () => {
-    const nextIndex = (currentIndex + 1) % videoRefs.value.length;
-    const nextVideo = videoRefs.value[nextIndex];
-    if (nextVideo) {
-      currentVideo.pause();
-      currentVideo.currentTime = 0;
-      nextVideo.muted = false;
-      nextVideo.play();
-      swiper.slideNext();
-    } else {
-      swiper.slideTo(0);
-    }
-  });
-};
-
-const playVideo = async () => {
-  if (videoRefs.value.length > 0) {
-    const firstVideo = videoRefs.value[0];
-    firstVideo.muted = false;
-
-    try {
-      await firstVideo.play();
-
-      // Add this code to play the audio with a delay and set the volume
-      if (audioRef.value) {
-        // Trick iOS into allowing playback after the delay
-        const AudioContext = window.AudioContext || window.webkitAudioContext;
-        const audioContext = new AudioContext();
-        const source = audioContext.createBufferSource();
-        source.connect(audioContext.destination);
-        source.start();
-
-        setTimeout(async () => {
-          audioRef.value.volume = 0.5;
-          try {
-            await audioRef.value.play();
-          } catch (error) {
-            console.error('Audio playback failed:', error);
-          }
-        }, 7000);
+const playVideo = (video) => {
+  if (video) {
+    //video.setVolume(isMuted.value ? 0 : 1);
+    video.play().catch((error) => {
+      if (
+        error.name === 'NotAllowedError' ||
+        error.name === 'NotSupportedError'
+      ) {
+        video.setVolume(0);
+        video.play();
       }
-    } catch (error) {
-      console.error('Video playback failed:', error);
-    }
-
-    firstVideo.addEventListener('ended', () => {
-      swiperEl.value.swiper.slideNext();
     });
   }
+};
+
+const togglePlay = () => {
+  if (swiperEl.value && swiperEl.value.swiper) {
+    const realIndex = swiperEl.value.swiper.realIndex;
+    const currentVideo = videoRefs.value[realIndex];
+    if (currentVideo) {
+      if (isPlaying.value) {
+        currentVideo.pause();
+        isPlaying.value = false;
+      } else {
+        playVideo(currentVideo);
+        isPlaying.value = true;
+        hasStartedPlaying.value = true;
+        currentPlayingIndex.value = realIndex; // Add this line
+      }
+    }
+  }
+};
+
+const toggleMute = () => {
+  if (swiperEl.value && swiperEl.value.swiper) {
+    const currentVideo = videoRefs.value[swiperEl.value.swiper.realIndex];
+    if (currentVideo) {
+      if (isMuted.value) {
+        currentVideo.muted = false;
+        isMuted.value = false;
+      } else {
+        currentVideo.muted = true;
+        isMuted.value = true;
+      }
+      showOverlay.value = window.innerWidth < 768 && isMuted.value;
+    }
+  }
+};
+
+const onSlideChange = (e) => {
+  console.log('slide changed');
+
+  // pause the previous video
+  if (currentPlayingIndex.value !== null) {
+    const previousVideo = videoRefs.value[currentPlayingIndex.value];
+    if (previousVideo) {
+      previousVideo.pause();
+    }
+  }
+
+  // play the current video
+  const currentVideo = videoRefs.value[e.detail[0].realIndex];
+  playVideo(currentVideo);
+  hasStartedPlaying.value = true;
+
+  // update the currently playing video index
+  currentPlayingIndex.value = e.detail[0].realIndex;
+
+  const currentIndex = e.detail[0].realIndex;
+  currentVideo.addEventListener('ended', function () {
+    const nextIndex = currentIndex + 1;
+    const nextVideo = videoRefs.value[nextIndex];
+    if (nextVideo) {
+      currentVideo.volume = 0;
+      playVideo(nextVideo);
+      swiperEl.value.swiper.slideNext();
+    } else {
+      swiperEl.value.swiper.slideTo(0);
+    }
+  });
 };
 </script>
 
@@ -153,7 +261,55 @@ const playVideo = async () => {
 body {
   margin: 0;
 }
+@media (max-width: 767px) {
+  swiper-slide > div {
+    width: 100%;
+    overflow: hidden;
+  }
+}
+iframe {
+  display: none !important;
+}
 .dynamic-player {
   height: 100vh;
+}
+.overlay {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.7);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 9;
+}
+.tooltip {
+  background-color: white;
+  color: black;
+  padding: 15px;
+  border-radius: 5px;
+  position: absolute;
+  top: 60px;
+  right: 10px;
+}
+.tooltip::after {
+  content: '';
+  position: absolute;
+  bottom: 100%;
+  right: 16px;
+  margin-left: -8px;
+  border-width: 8px;
+  border-style: solid;
+  border-color: transparent transparent white transparent;
+}
+.hide-unless-hovered {
+  opacity: 0;
+  transition: opacity 0.25s ease-in-out;
+}
+.hide-unless-hovered:hover,
+swiper-slide > video:hover + .hide-unless-hovered {
+  opacity: 1;
 }
 </style>
